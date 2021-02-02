bdfe5a6c7dd45d5c8658c5e4ba2d9042378e8b1e
Anne <anne.kreuter@outlook.de>
2021-02-02 18:37:34 +0100

    Dynamisches Laden der Social Media Profiles

--- PycharmProjects/Anxiety/BaselineModel/main.py
+++ PycharmProjects/Anxiety/BaselineModel/main.py
@@ -1,4 +1,6 @@
 import json
+import os
+import random
 import statistics
 import time
 from absl import app, flags, logging
@@ -13,15 +15,16 @@ from random import randint
 from transformers import pipeline
 import re
 
-flags.DEFINE_integer('epochs', 2, '')
+flags.DEFINE_integer('epochs', 10, '')
 flags.DEFINE_integer('batch_size', 32, '')
 flags.DEFINE_float('lr', 1e-2, '')
 flags.DEFINE_float('momentum', .9, '')
 flags.DEFINE_string('model', 'bert-base-uncased', '')
 flags.DEFINE_boolean('debug', False, '')
 flags.DEFINE_boolean('augmentation', False, '')
-flags.DEFINE_string('concept', 'Anxiety', '' )
-flags.DEFINE_boolean('balance', False, '')
+flags.DEFINE_string('concept', 'Depression', '' )
+flags.DEFINE_boolean('balance', True, '')
+flags.DEFINE_float('threshold', 0.4, '')
 
 FLAGS = flags.FLAGS
 
@@ -46,6 +49,7 @@ class BertModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         if FLAGS.concept == 'NEOPIP':
             batch['label'] = batch['label'].type(th.FloatTensor)
+        print("train with: ", batch['label'])
         logits = self.forward(batch['input_ids'])
         loss = self.loss(logits, batch['label']).mean()
         return {'loss': loss, 'log': {'train_loss': loss}}
@@ -55,20 +59,19 @@ class BertModel(pl.LightningModule):
             batch['label'] = batch['label'].type(th.FloatTensor)
         logits = self.forward(batch['input_ids'])
         loss = self.loss(logits,batch['label'])
-        print("batch['label']: ", batch['label'])
-        print("logits: ", logits)
         if FLAGS.concept == 'NEOPIP':
+            logits[logits < 0.5] = 0
+            logits[logits >= 0.5] = 1
             acc = (logits.round() == batch['label']).float()
-            TP = th.sum(((logits.round() == 1) & (batch['label'] == 1)).float(), dim=1)
-            print("TP: ", TP)
-            FP = th.sum(((logits.round() == 1) & (batch['label'] == 0)).float(), dim=1)
-            print("FP: ", FP)
-            FN = th.sum(((logits.round() == 0) & (batch['label'] == 1)).float(), dim=1)
+            TP = th.sum(((logits == 1) & (batch['label'] == 1)).float(), dim=1)
+            FP = th.sum(((logits == 1) & (batch['label'] == 0)).float(), dim=1)
+            FN = th.sum(((logits == 0) & (batch['label'] == 1)).float(), dim=1)
             prec = TP / (FP + TP)
             prec = prec.mean()
             prec[th.isnan(prec)] = 0.
             rec = (TP / (TP + FN)).mean()
             rec[th.isnan(rec)] = 0.
+            pred = sum(logits.argmax(-1) == 1)
         else:
             TP = sum(((logits.argmax(-1) == 1) & (batch['label'] == 1)).int())
             FP = sum(((logits.argmax(-1) == 1) & (batch['label'] == 0)).int())
@@ -78,9 +81,10 @@ class BertModel(pl.LightningModule):
             prec[th.isnan(prec)] = 0.
             rec = TP / (TP + FN)
             rec[th.isnan(rec)] = 0.
+            pred = sum(logits.argmax(-1) == 1)
         f1 = (2 * prec * rec) / (prec + rec)
         f1[th.isnan(f1)] = 0.
-        return {'loss': loss, 'acc': acc, 'prec': prec, 'rec': rec, 'f1': f1}
+        return {'loss': loss, 'acc': acc, 'prec': prec, 'rec': rec, 'f1': f1, 'pred': pred}
 
     def validation_epoch_end(self, outputs):
         loss = th.cat([o['loss'] for o in outputs], 0).mean()
@@ -91,7 +95,13 @@ class BertModel(pl.LightningModule):
         rec = th.mean(th.stack(rec))
         f1 = [o['f1'] for o in outputs]
         f1 = th.mean(th.stack(f1))
-        self.out = {'val_loss': loss, 'val_acc': acc, 'val_prec': prec, 'val_rec': rec, 'val_f1': f1}
+        pred = [o['pred'] for o in outputs]
+        print(pred)
+        pred = th.sum(th.stack(pred))
+        print(pred)
+        pred = pred / len(self.ds_test)
+        print(pred)
+        self.out = {'val_loss': loss, 'val_acc': acc, 'val_prec': prec, 'val_rec': rec, 'val_f1': f1, 'val_pred': pred}
         print("out: ", self.out)
         return {**self.out, 'log': self.out}
 
@@ -121,7 +131,7 @@ class BertModel(pl.LightningModule):
 
 
 
-def _prepare_ds():
+def _prepare_ds(user):
     tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
     fillmask = pipeline('fill-mask', top_k=3)
     mask_token = fillmask.tokenizer.mask_token
@@ -205,9 +215,6 @@ def _prepare_ds():
             augmented['label'] += label
             counter -= 1
             print("added ", len(sequence), " augmentations to item.", counter, "to go")
-            import IPython;
-            IPython.embed();
-            exit(1)
         return augmented
 
     def _get_more_control_samples(split):
@@ -232,14 +239,14 @@ def _prepare_ds():
                 K = randint(0, len(control_items["data"]) - 1)
                 control_item = control_items["data"][K]
                 added_control_samples['item'].append(control_item['item'])
-                added_control_samples['label'].append(0)
+                added_control_samples['label'].append(control_item['label'])
 
         print("Successfully added ", missing_control, " control samples.")
         return added_control_samples
 
 
-    def _load_dataset():
-        ds_test = load_dataset('json', data_files=r'C:\Users\Anne\AppData\Roaming\JetBrains\PyCharmCE2020.2\scratches\%s.json' %FLAGS.concept, field='data', split=[f'train[{k}%:{k+10}%]' for k in range(0, 100, 10)])
+    def _load_dataset(user):
+        ds_test = Dataset.from_dict(user)
         ds_train = load_dataset('json', data_files=r'C:\Users\Anne\AppData\Roaming\JetBrains\PyCharmCE2020.2\scratches\%s.json' %FLAGS.concept, field='data', split=[f'train[:{k}%]+train[{k+10}%:]' for k in range(0, 100, 10)])
         list_of_ds_train = []
         list_of_ds_test = []
@@ -251,14 +258,11 @@ def _prepare_ds():
             split = split.map(_tokenize)
             split.set_format(type='torch', columns=['input_ids', 'label'])
             list_of_ds_train.append(split)
-        for split in ds_test:
-            if FLAGS.augmentation:
-                split = Dataset.from_dict(_get_augmentation(split))
-            if FLAGS.balance:
-                split = Dataset.from_dict(_get_more_control_samples(split))
-            split = split.map(_tokenize)
-            split.set_format(type='torch', columns=['input_ids', 'label'])
-            list_of_ds_test.append(split)
+        ds_test = ds_test.map(_tokenize)
+        ds_test.set_format(type='torch', columns=['input_ids', 'label'])
+        for i in range(10):
+            print(i)
+            list_of_ds_test.append(ds_test)
         return list_of_ds_train, list_of_ds_test
 
     def _tokenize(x):
@@ -269,21 +273,20 @@ def _prepare_ds():
         )
         return x
 
-    list_of_ds_train, list_of_ds_test = _load_dataset()
+    list_of_ds_train, list_of_ds_test = _load_dataset(user)
     return list_of_ds_train, list_of_ds_test
 
-def main(_):
-    start = time.time()
-    list_of_ds_train, list_of_ds_test = _prepare_ds()
+def give_me_label(list_of_ds_train, list_of_ds_test):
     performance = {}
     performance['mse'] = []
     performance['acc'] = []
     performance['prec'] = []
     performance['rec'] = []
     performance['f1'] = []
+    performance['pred'] = []
 
     for i in range(len(list_of_ds_train)):
-        print("******************* Round No. ", i+1, " *******************")
+        print("******************* Round No. ", i + 1, " *******************")
         model = BertModel()
         model.ds_train = list_of_ds_train[i]
         print(model.ds_train)
@@ -304,13 +307,81 @@ def main(_):
         prec = model.out['val_prec']
         rec = model.out['val_rec']
         f1 = model.out['val_f1']
+        pred = model.out['val_pred']
         performance['acc'].append(acc.item())
         performance['prec'].append(prec.item())
         performance['rec'].append(rec.item())
         performance['f1'].append(f1.item())
+        if pred > FLAGS.threshold:
+            performance['pred'].append(1)
+        else:
+            performance['pred'].append(0)
+    return performance
 
-    for key, values in performance.items():
-        print(key, ": ", statistics.mean(values))
+def get_users():
+    print("*****Laden der Social Media Profiles*****")
+    user_profiles = []
+    for subdir, dirs, files in os.walk(r'C:\Users\Anne\Documents\Masterarbeit\Datasets\Twitter_API\%s' %FLAGS.concept):
+        for file in files:
+            profile_dict = {}
+            profile_dict["item"] = []
+            profile_dict["label"] = []
+            path_profile = os.path.join(subdir, file)
+            print(path_profile)
+            profile = open(path_profile, encoding='utf8')
+            profile = json.load(profile)
+            for k, v in profile.items():
+                profile_dict["item"].append(v['0'])
+                profile_dict["label"].append(1)
+            user_profiles.append(profile_dict)
+    for subdir, dirs, files in os.walk(r'C:\Users\Anne\Documents\Masterarbeit\Datasets\Twitter_API\Control'):
+        for file in files:
+            profile_dict = {}
+            profile_dict["item"] = []
+            profile_dict["label"] = []
+            path_profile = os.path.join(subdir, file)
+            print(path_profile)
+            profile = open(path_profile, encoding='utf8')
+            profile = json.load(profile)
+            for k, v in profile.items():
+                profile_dict["item"].append(v['0'])
+                profile_dict["label"].append(0)
+            user_profiles.append(profile_dict)
+    random.shuffle(user_profiles)
+    print(user_profiles)
+    return user_profiles
+
+
+def main(_):
+    start = time.time()
+    user_profiles = get_users()
+    TP = 0
+    FP = 0
+    FN = 0
+    for user in user_profiles:
+        print(user.get("label")[0])
+        list_of_ds_train, list_of_ds_test = _prepare_ds(user)
+        performance = give_me_label(list_of_ds_train, list_of_ds_test)
+        print("performance: ", performance)
+        for key, values in performance.items():
+            print(key, ": ", statistics.mean(values))
+
+        y_pred = max(set(performance['pred']), key = performance['pred'].count) #most frequent label
+        print("user labeled as: ", y_pred)
+
+        if y_pred == 1 and user.get("label")[0] == 1:
+            TP += 1
+        elif y_pred == 0 and user.get("label")[0] == 1:
+            FN += 1
+        elif y_pred == 1 and user.get("label")[0] == 0:
+            FP += 1
+
+    Precision = TP / (TP + FP)
+    print("Precision: ", Precision)
+    Recall = TP / (TP + FN)
+    print("Recall: ", Recall)
+    F1 = (2*Precision*Recall)/(Recall + Precision)
+    print("F1: ", F1)
 
     ende = time.time()
     print('{:5.3f}s'.format(ende - start))
